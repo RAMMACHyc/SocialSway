b9b5295b0138bfff6fbb1ee22507e53b22dbce88
RAMMACHyc <sadik.ram12@gmail.com>
2024-02-04 23:46:45 +0100

    Add MainContainer component with bottom tab navigation

---
+++ app/MainContainer.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SettingsScreen from './screens/SettingsScreen';
+import Icon from '@expo/vector-icons/Ionicons';
+import DrawerNavigator from './DrawerNavigator';
+
+
+
+const homeName = "Home";
+const settingsName = "Favorite";
+
+const Tab = createBottomTabNavigator();
+
+
+function MainContainer() {
+
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName={homeName}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let rn = route.name;
+  
+              if (rn === homeName) {
+                iconName = focused ? 'home' : 'home-outline';
+
+  
+              } else if (rn === settingsName) {
+                iconName = focused ? 'heart' : 'heart-outline';
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'grey',
+            tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
+            tabBarStyle: { padding: 10, height: 70 }
+          })}>
+  
+          <Tab.Screen name={homeName} component={DrawerNavigator} options={{headerShown: false}} />
+          <Tab.Screen name={settingsName} component={SettingsScreen} />
+          
+
+  
+        </Tab.Navigator>
+
+
+      </NavigationContainer>
+    );
+  }
+
+export default MainContainer;
